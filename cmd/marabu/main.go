b8a1655a0eeff2281e7e5915f38a6a7c4a3177ec
Panos Kaf <panoskatsalifis@gmail.com>
2026-03-13 17:57:36 +0200

    start server and connect to bootstrap nodes

--- cmd/marabu/main.go
+++ cmd/marabu/main.go
@@ -1,5 +1,30 @@
 package main
 
-import ()
+import (
+	"fmt"
+	"marabu/internal/peer"
+	"net"
+	"strconv"
+)
 
-func main() {}
+func main() {
+
+	go peer.StartServer(18018)
+
+	for _, p := range peer.BOOTSTRAP_PEERS {
+		go func(p string) {
+			host, portStr, _ := net.SplitHostPort(p)
+			port, _ := strconv.Atoi(portStr)
+			err := peer.StartClient(host, port, 0, func() {
+				// Handle client disconnect if needed
+			})
+			fmt.Println("Connected to peer: " + p)
+			if err != nil {
+				fmt.Printf("Error connecting to peer %s: %v\n", p, err)
+			}
+		}(p)
+	}
+
+	select {}
+
+}
