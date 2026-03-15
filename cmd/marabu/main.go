e20476bce5486405cdeb334f40b5c08f9f951c1b
Panos Kaf <panoskatsalifis@gmail.com>
2026-03-15 20:20:25 +0200

    cli + log setup

--- cmd/marabu/main.go
+++ cmd/marabu/main.go
@@ -2,29 +2,40 @@ package main
 
 import (
 	"fmt"
+	"marabu/internal/cli"
+	"marabu/internal/object"
 	"marabu/internal/peer"
+	"marabu/internal/logs"
 	"net"
 	"strconv"
 )
 
 func main() {
 
-	go peer.StartServer(18018)
+	logFile := logs.InitLogs()
+	defer logFile.Close()
+
+	objectManager, err := object.NewObjectManager("./db")
+	if err != nil {
+		fmt.Printf("Error creating object manager: %v\n", err)
+		return
+	}
+
+	go peer.StartServer(18018, objectManager)
 
 	for _, p := range peer.BOOTSTRAP_PEERS {
 		go func(p string) {
 			host, portStr, _ := net.SplitHostPort(p)
 			port, _ := strconv.Atoi(portStr)
-			err := peer.StartClient(host, port, 0, func() {
+			err := peer.StartClient(host, port, objectManager, func() {
 				// Handle client disconnect if needed
 			})
-			fmt.Println("Connected to peer: " + p)
 			if err != nil {
 				fmt.Printf("Error connecting to peer %s: %v\n", p, err)
 			}
 		}(p)
 	}
 
-	select {}
+	cli.Start()
 
 }
