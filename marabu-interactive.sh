89e62746800923e95ab07cc406f50a55cf2efd30
Panos Kaf <panoskatsalifis@gmail.com>
2026-03-15 20:08:45 +0200

    script improvements

--- marabu-interactive.sh
+++ marabu-interactive.sh
@@ -5,7 +5,7 @@ marabu="./marabu"
 # Make sure we're running inside kitty
 if [ -z "$KITTY_WINDOW_ID" ]; then
   echo "Error: This script should be run inside a kitty terminal. Fallback to CLI only."
-  $marabu
+  exec $marabu
   exit 1
 fi
 
@@ -13,18 +13,15 @@ FILE="${HOME}/dev/blockchain/marabu/logs/latest.log"
 
 > $FILE
 
-# get id of the current window
-cliWindowID=$KITTY_WINDOW_ID
-
 # Launch a new kitty window for logs and capture the window ID
-logsWindowID=$(kitty @ launch --title "Marabu Logs" bash -c "tail -n +1 -F ${FILE} 2>/dev/null")
+logsWindowID=$(kitty @ launch \
+  --title "Marabu Logs" \
+  --keep-focus \
+  bash -c "tail -n +1 -F '$FILE' 2>/dev/null")
 
-kitty @ focus-window --match id:$cliWindowID
+trap "kitty @ close-window --match id:$logsWindowID" EXIT
 
 # Run CLI in current terminal
 $marabu
 CLI_PID=$!
 wait $CLI_PID
-
-# When CLI exits, close the log window
-kitty @ close-window --match id:$logsWindowID
