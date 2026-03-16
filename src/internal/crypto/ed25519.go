c1c24435168a1baff5f852d4fba306fb9555f6f2
Panos Kaf <panoskatsalifis@gmail.com>
2026-03-16 19:23:00 +0200

    convert to ed25519.PublicKey type

--- src/internal/crypto/ed25519.go
+++ src/internal/crypto/ed25519.go
@@ -14,3 +14,15 @@ func Sign(message []byte, privateKey ed25519.PrivateKey) (string, error) {
 func Verify(message []byte, signature string, publicKey ed25519.PublicKey) bool {
 	return ed25519.Verify(publicKey, message, []byte(signature))
 }
+
+func DecodeString(hexStr string) ([]byte, error) {
+	return []byte(hexStr), nil
+}
+
+func StringToPubkey(hexStr string) (ed25519.PublicKey, error) {
+	pubkeyBytes, err := DecodeString(hexStr)
+	if err != nil {
+		return nil, err
+	}
+	return ed25519.PublicKey(pubkeyBytes), nil
+}
