739f3135d504e1bb81e1008592d233a88ffdab53
Panos Kaf <panoskatsalifis@gmail.com>
2026-03-17 01:37:45 +0200

    change verify interface

--- src/internal/crypto/ed25519.go
+++ src/internal/crypto/ed25519.go
@@ -2,21 +2,31 @@ package crypto
 
 import (
 	"crypto/ed25519"
+	"encoding/hex"
 )
 
 // Sign signs the given message using the provided Ed25519 private key and returns the signature as a hexadecimal string.
 func Sign(message []byte, privateKey ed25519.PrivateKey) (string, error) {
 	signature := ed25519.Sign(privateKey, message)
-	return string(signature), nil
+	return hex.EncodeToString(signature), nil
 }
 
 // Verify verifies the given signature for the provided message and Ed25519 public key. It returns true if the signature is valid, false otherwise.
-func Verify(message []byte, signature string, publicKey ed25519.PublicKey) bool {
-	return ed25519.Verify(publicKey, message, []byte(signature))
+func Verify(publicKey string, message []byte, signature string) bool {
+
+	pubkey, err := StringToPubkey(publicKey)
+	if err != nil {
+		return false
+	}
+	sigBytes, err := hex.DecodeString(signature)
+	if err != nil {
+		return false
+	}
+	return ed25519.Verify(pubkey, message, sigBytes)
 }
 
 func DecodeString(hexStr string) ([]byte, error) {
-	return []byte(hexStr), nil
+	return hex.DecodeString(hexStr)
 }
 
 func StringToPubkey(hexStr string) (ed25519.PublicKey, error) {
