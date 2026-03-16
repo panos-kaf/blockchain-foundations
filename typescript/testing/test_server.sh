60126052152008447a95ff6cd9c2fc5e99f69397
Panos Kaf <panoskatsalifis@gmail.com>
2026-03-16 19:05:18 +0200

    rm ts implementation

--- typescript/testing/test_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-clients=$1
-
-for (( i=1; i<=clients; i++ )); do 
-(echo "{\"type\":\"hello\",\"agent\":\"script\",\"version\":\"0.10.0\"}"; echo "{\"type\":\"getpeers\"}"; sleep 2) | nc localhost 18018 & done
